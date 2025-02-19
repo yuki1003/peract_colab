c49ddcd32a68cc0e02e1526d035f86ee2fc5b6b0
Yuki Watabe <yuki1234@hotmail.nl>
2025-02-19 10:53:11 +0100

    update the gripper mesh path so it works on server

--- arm/utils.py
+++ arm/utils.py
@@ -234,7 +234,10 @@ def visualise_voxel(voxel_grid: np.ndarray,
         s.add(cam, pose=t.pose)
 
         if render_gripper:
-            gripper_trimesh = trimesh.load('peract_colab/meshes/hand.dae', force='mesh')
+            try:
+                gripper_trimesh = trimesh.load('peract_colab/meshes/hand.dae', force='mesh')
+            except:
+                gripper_trimesh = trimesh.load('/home/bepgroup/Projects/PerAct_ws/peract_colab/meshes/hand.dae', force='mesh')
             gripper_trimesh.vertices *= gripper_mesh_scale
             radii = np.linalg.norm(gripper_trimesh.vertices - gripper_trimesh.center_mass, axis=1)
             gripper_trimesh.visual.vertex_colors = trimesh.visual.interpolate(radii * gripper_mesh_scale, color_map='winter')
